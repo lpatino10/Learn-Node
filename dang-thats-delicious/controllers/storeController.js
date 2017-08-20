f809bd18477579959cec40fa9f1e2ba9d8a66e1a
Logan Patino <loganpatino10@gmail.com>
2017-08-20 17:38:07 -0400

    Add file uploading and start of single-store view

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+    storage: multer.memoryStorage(),
+    fileFilter(req, file, next) {
+        const isPhoto = file.mimetype.startsWith('image/');
+        if (isPhoto) {
+            next(null, true);
+        }
+        else {
+            next({ message: 'That filetype isn\'t allowed!' }, false);
+        }
+    }
+};
 
 exports.homePage = (req, res) => {
     console.log(req.name);
@@ -9,6 +25,23 @@ exports.homePage = (req, res) => {
 exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
 };
+
+exports.resize = async (req, res, next) => {
+    // check if there is no new file to resize
+    if (!req.file) {
+        next(); // skip to next middleware
+        return;
+    }
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
+    // now we resize
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`./public/uploads/${req.body.photo}`);
+    next();
+};
+
+exports.upload = multer(multerOptions).single('photo');
 
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
@@ -41,3 +74,9 @@ exports.updateStore = async (req, res) => {
     req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
     res.redirect(`/stores/${store._id}/edit`);
 }
+
+exports.getStoreBySlug = async (req, res, next) => {
+    const store = await Store.findOne({ slug: req.params.slug });
+    if (!store) return next();
+    res.render('store', { store, title: store.name });
+};
