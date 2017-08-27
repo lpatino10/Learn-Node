b713bda7b1f19430e1958b8d313cde8de1c5cb1e
Logan Patino <loganpatino10@gmail.com>
2017-08-27 17:42:17 -0400

    Add db querying for store tags

--- dang-thats-delicious/models/Store.js
+++ dang-thats-delicious/models/Store.js
@@ -35,14 +35,30 @@ const storeSchema = new mongoose.Schema({
     photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
     if (!this.isModified('name')) {
         next(); // skip
         return;
     }
     this.slug = slug(this.name);
+
+    // find other stores with slugs like test, test-1, etc.
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+    const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+    if (storesWithSlug.length) {
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
+
     next();
     // TODO: make sure slugs are unique
 });
+
+storeSchema.statics.getTagsList = function() {
+    return this.aggregate([
+        { $unwind: '$tags' },
+        { $group: { _id: '$tags', count: { $sum: 1 } } },
+        { $sort: { count: -1 } }
+    ]);
+};
 
 module.exports = mongoose.model('Store', storeSchema);
