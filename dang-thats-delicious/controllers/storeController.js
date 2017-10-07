60b9aba745c263b1d1c2eac3e8c38929ae747126
Logan Patino <loganpatino10@gmail.com>
2017-10-07 18:43:37 -0400

    Add review list and top store list

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -84,7 +84,7 @@ exports.updateStore = async (req, res) => {
 }
 
 exports.getStoreBySlug = async (req, res, next) => {
-    const store = await Store.findOne({ slug: req.params.slug }).populate('author');
+    const store = await Store.findOne({ slug: req.params.slug }).populate('author reviews');
     if (!store) return next();
     res.render('store', { store, title: store.name });
 };
@@ -153,3 +153,8 @@ exports.getHearts = async (req, res) => {
     });
     res.render('stores', { title: 'Hearted Stores', stores });
 };
+
+exports.getTopStores = async (req, res) => {
+    const stores = await Store.getTopStores();
+    res.render('topStores', { stores, title:'★ Top Stores!' });
+}
